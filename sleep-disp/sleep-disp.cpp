8fb31f5aac8bc6ef847ea4a1224f6376c6ac18b6
wilhellmina <x@wakiga.nl>
2021-11-19 12:16:48 +0900

    w

--- sleep-disp/sleep-disp.cpp
+++ sleep-disp/sleep-disp.cpp
@@ -1,9 +1,27 @@
 ﻿#include <windows.h>
+#include <chrono>
+#include <thread>
 
 #define SET_SLEEP 2
+#define DECIMAL 10
 
-int main() {
+int main(int argc, char *argv[]) {
+	int64_t sleep_ms;
 	bool ret;
+
+	if (argc < 2) {
+		sleep_ms = 3250;
+	}
+	else {
+		const char* const str = argv[1];
+		char *last;
+		sleep_ms = strtoll(str, &last, DECIMAL);
+		if ('\0' != *last) {
+			return EXIT_FAILURE;
+		}
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
 	ret = PostMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, SET_SLEEP);
 	
 #if 0
@@ -15,5 +33,5 @@ int main() {
 
 #endif
 
-	return 0;
+	return EXIT_SUCCESS;
 }
